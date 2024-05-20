7ea21b6988ab299c79e025ac31af6a5f8ac1d17e
Julio H Cavalcanti <juliophc@msn.com>
2024-05-19 22:21:07 -0300

    erro em conexao

--- python/main.py
+++ python/main.py
@@ -1,22 +1,69 @@
 import mysql.connector
+import streamlit as st
+import pandas as pd
 
 connection = mysql.connector.connect(
-    user='root', password='root', host='mysql', port="3306", database='db')
+    user='root',
+    password='root',
+    host='127.0.0.1',
+    port="3306",
+    database='db'
+    )
+
 print("DB connected")
 
-connection.close()
 cursor = connection.cursor()
 
-aviao_id = 1
-aviao_modelo = 'Boeing 737'
+def close_connection():
+    cursor.close()
+    connection.close()
 
-insert_query = """
-INSERT INTO aviao (id, modelo)
-VALUES (%s, %s)
-"""
-cursor.execute(insert_query, (aviao_id, aviao_modelo))
-connection.commit()
+def insert_data(table, data):
+    placeholders = ', '.join(['%s'] * len(data))
+    query = f"INSERT INTO {table} VALUES ({placeholders})"
+    cursor.execute(query, data)
+    connection.commit()
+    st.success(f'Dados inseridos em {table}.')
 
-cursor.execute('Select * FROM aviao')
-avioes = cursor.fetchall()
-print(avioes)
+st.title("Aplicação de Gerenciamento de Dados")
+option = st.sidebar.selectbox("Selecione a operação", ("Inserir", "Deletar", "Atualizar", "Select"))
+table = st.sidebar.selectbox("Selecione a tabela", ("aviao", "destinos", "funcionario", "passageiro", "reserva", "voo"))
+
+if option == "Inserir":
+    if table == "aviao":
+        id_aviao = st.number_input("Id do avião", min_value=0)
+        modelo_aviao = st.text_input("Modelo do avião")
+        if st.button("Inserir"):
+            insert_data(table, (id_aviao, modelo_aviao))
+    elif table == "destinos":
+        id_destino = st.number_input("Id do destino", min_value=0)
+        nome_destino = st.text_input("Nome do destino")
+        if st.button("Inserir"):
+            insert_data(table, (id_destino, nome_destino))
+    elif table == "funcionario":
+        cpf_funcionario = st.text_input("Cpf do funcionário")
+        nome_funcionario = st.text_input("Nome do funcionário")
+        cargo_funcionario = st.text_input("Cargo do funcionário")
+        aviao_funcionario = st.number_input("Id do avião", min_value=0)
+        if st.button("Inserir"):
+            insert_data(table, (cpf_funcionario, nome_funcionario, cargo_funcionario, aviao_funcionario))
+    elif table == "passageiro":
+        cpf_passageiro = st.text_input("Cpf do passageiro")
+        nome_passageiro = st.text_input("Nome do passageiro")
+        if st.button("Inserir"):
+            insert_data(table, (cpf_passageiro, nome_passageiro))
+    elif table == "reserva":
+        id_voo = st.number_input("Id do Vôo", min_value=0)
+        cpf_reserva = st.text_input("Cpf do passageiro")
+        if st.button("Inserir"):
+            insert_data(table, (cpf_reserva, id_voo))
+    elif table == "voo":
+        id_voo = st.number_input("Id do Vôo", min_value=0)
+        destino_voo = st.number_input("Id do destino", min_value=0)
+        aviao_voo = st.number_input("Id do avião", min_value=0)
+        if st.button("Inserir"):
+            insert_data(table, (id_voo, destino_voo, aviao_voo))
+
+
+
+st.sidebar.button("Fechar conexão", on_click=close_connection)
